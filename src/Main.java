354acc2ab81aabdea2f32f41d6ac320c15f497bc
Gleb <demonxklimenko@mail.ru>
2021-05-30 19:19:32 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        float tiket = 2_500;
+
+        int mili = (int) tiket / 20;
+        System.out.println(" На счету" + mili + " миль");
+    }
+}
+
+
+
+
+
+
+
+
+
+
